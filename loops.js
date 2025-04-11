5c74873f09f300c20ab8e7072e0a305aa5df0790
ianjiteshan <anjitesh1976@gmail.com>
2025-04-11 06:55:03 +0530

    Added DOM files

--- loops.js
+++ loops.js
@@ -34,8 +34,7 @@ for (let i = 0; i < 5; i++) {
     if (i == 3) {
         break;
     }
-    console.log(i);
-}
+    console.log(i); }
 
 //continue
 console.log("continue");
@@ -45,3 +44,57 @@ for (let i = 0; i < 5; i++) {
     }
     console.log(i);
 }
+
+//for of-array and other iterables
+console.log("forof");
+let o = [1, 2, 3, 4, 5];
+console.log(o);
+for (const c of o) {
+    console.log(c);
+}
+
+//for in-object 
+console.log("forin");
+let o1 = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+};
+console.log(o1);
+for (const c in o1) {
+    console.log(c);
+}
+
+//map
+console.log("map");
+let o2 = new Map();
+o2.set("a", 1);
+o2.set("b", 2);
+o2.set("c", 3);
+o2.set("d", 4);
+o2.set("e", 5);
+console.log(o2);
+for (const [key, value] of o2) {
+    console.log(key,"has", value);
+}
+
+//for each
+console.log("foreach");
+let o3 = [
+    {
+        username: "Anji",
+        age: 20,
+    },
+    {
+        username: "Shan",
+        age: 20,
+    },
+    {
+        username: "Rishav",}
+];
+console.log(o3);
+o3.forEach((c) => {
+    console.log(c.username);
+})
